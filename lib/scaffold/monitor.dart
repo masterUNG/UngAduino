77bbb39b3a9784293d2efc687f23686916ab2b16
masterUNG <phrombutr@gmail.com>
2019-12-30 13:32:06 +0700

    detact 3 Success

--- lib/scaffold/monitor.dart
+++ lib/scaffold/monitor.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +12,7 @@ class _MonitorState extends State<Monitor> {
   // Field
   String despt;
   int status;
+  bool statusQRcode = true;
 
   // Method
 
@@ -26,14 +29,38 @@ class _MonitorState extends State<Monitor> {
         .child('Line_01')
         .child('System')
         .child('Mobile');
-        await databaseReference.once().then((DataSnapshot dataSnapshot){
-          print('dataSnapshot ===>>> ${dataSnapshot.value}');
-          Map<dynamic, dynamic> map = dataSnapshot.value;
-          setState(() {
-            despt = map['despt'];
-            status = map['status'];
-          });
-        });
+    await databaseReference.once().then((DataSnapshot dataSnapshot) {
+      // print('dataSnapshot ===>>> ${dataSnapshot.value}');
+      Map<dynamic, dynamic> map = dataSnapshot.value;
+      setState(() {
+        despt = map['despt'];
+        status = map['status'];
+        if (statusQRcode) {
+          qrAndBarCode();
+        }
+      });
+      myDuration();
+    });
+  }
+
+  Future<void> qrAndBarCode() async {
+    if (status == 3) {
+      statusQRcode = false;
+      print('Open Scan');
+    }
+  }
+
+  Future<void> myDuration() async {
+    Duration duration = Duration(seconds: 3);
+    Timer(duration, () {
+      return readDataThread();
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    readDataThread();
   }
 
   Widget showProgress() {
